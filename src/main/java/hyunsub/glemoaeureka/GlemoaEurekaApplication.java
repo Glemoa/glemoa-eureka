4457ef511aa384bf58c67c3ba73c5feae19c2ab3
신현섭 <didakd22@naver.com>
2025-09-30 22:17:49 +0900

    first commit

---
+++ src/main/java/hyunsub/glemoaeureka/GlemoaEurekaApplication.java
@@ -0,0 +1,13 @@
+package hyunsub.glemoaeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlemoaEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlemoaEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hyunsub/glemoaeureka/GlemoaEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package hyunsub.glemoaeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlemoaEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
